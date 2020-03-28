a2d2ea647b9e09e2d170dcea4905860655c46db1
Sean Kotrola <kotrolass@vcu.edu>
2020-03-28 14:18:27 -0400

    Header Things

--- Web/covid_simulation/src/Components/Main/Header/Header.js
+++ Web/covid_simulation/src/Components/Main/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './Header.css'
 import Navbar from 'react-bootstrap/Navbar'
+import covid from '../../../images/covidPic.png'
 
 class Header extends Component {
 
@@ -8,8 +9,21 @@ class Header extends Component {
     render() {
         return (
             <div className='headerContainer'>
-                <Navbar bg="transparent" expand="lg" style = {{height: '100%'}}>
-                    <Navbar.Brand href="#home" >COVID-19</Navbar.Brand>
+                <Navbar bg="transparent" expand="lg" style={{ height: '100%' }}>
+                    <Navbar.Brand href="#home" >
+                        <div className='headerLogo'>
+                            <img
+                                alt=""
+                                src={covid}
+                                width="60"
+                                height="60"
+                                className="d-inline-block align-top imgSpacing"
+                            />
+                            <div className = 'headerLogoText'>
+                                Insert Name of website here
+                            </div>
+                        </div>
+                    </Navbar.Brand>
                 </Navbar>
             </div>
         );
